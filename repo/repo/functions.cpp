4150ed83f05154f5aa57709e08b606a889d5f6e1
Yura <krulikyura228@gmail.com>
2019-05-17 23:56:11 +0300

    Files added to project

---
+++ repo/repo/functions.cpp
@@ -0,0 +1,5 @@
+#include "Header.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
---
+++ repo/repo/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "Header.h"
+using namespace std;
+int main(){}
